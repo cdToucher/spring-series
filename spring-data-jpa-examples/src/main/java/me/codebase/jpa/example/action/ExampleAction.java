c51f5c503fdb33d6d6c2d5177ac704557265f944
chendong <chendong@broada.com>
2017-05-22 16:55:23 +0800

    first commit;

--- spring-data-jpa-examples/src/main/java/me/codebase/jpa/example/action/ExampleAction.java
+++ spring-data-jpa-examples/src/main/java/me/codebase/jpa/example/action/ExampleAction.java
@@ -4,6 +4,9 @@ import me.codebase.jpa.example.entity.User;
 import me.codebase.jpa.example.repository.ExampleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -32,8 +35,14 @@ public class ExampleAction {
 //        repository.save(user);
 //        List<User> list = repository.findAll();
 //        list.forEach(System.out::println);
+
+        //        repository.getOne(1L)  ?? 这里使用getOne 会报错!
         User user1 = repository.findOne(1L);
-//        repository.getOne(1L)  ?? 这里使用getOne 会报错!
         System.out.println(user1.toString());
+        User user2 = repository.findByName("test");
+        System.out.println(user2.toString());
+        Pageable pageable = new PageRequest(1, 5);
+        Page<User> page = repository.findAllByName("test", pageable);
+        System.out.println(page.getTotalElements());
     }
 }
--- spring-data-jpa-examples/src/main/java/me/codebase/jpa/example/repository/ExampleRepository.java
+++ spring-data-jpa-examples/src/main/java/me/codebase/jpa/example/repository/ExampleRepository.java
@@ -2,10 +2,22 @@ package me.codebase.jpa.example.repository;
 
 
 import me.codebase.jpa.example.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by chendong on 2017/5/19.
  */
 public interface ExampleRepository extends JpaRepository<User, Long> {
+
+    User findByName(String userName);
+
+    Page<User> findAllByName(String userName, Pageable pageable);
+
+    @Transactional
+    @Query
+    int queryByMyself();
 }
